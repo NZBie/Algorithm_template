293304fa27d0d7b9420ab3eaf152468310519ad1
NZB <569019895@qq.com>
2022-05-03 00:43:00 +0800

    add DSU

--- DisjointSetUnion/DSU.cpp
+++ DisjointSetUnion/DSU.cpp
@@ -2,7 +2,7 @@
  * @Author: NZB 
  * @Date: 2022-04-29 22:38:31 
  * @Last Modified by: NZB
- * @Last Modified time: 2022-05-01 22:05:06
+ * @Last Modified time: 2022-05-03 00:42:03
  */
 
 #include "DSU.h"
@@ -14,14 +14,16 @@ Trace error(true);
 
 /**
  *	@brief	并查集 DSU类 的构造函数
- *	@param	Tsz setSize		: 并查集容量
+ *	@param	Tsz unionSize	: 并查集容量
 **/
-DSU::DSU(Tsz setSize): 
-_setSize((setSize > max_size) ? 0 : setSize), _parId(new Tsz[_setSize]), _rank(new Tsz[_setSize]) {
-	if(setSize > max_size) error.print("Error: \"dataSize\"参数设置过大，不能超过\"max_size\"！");
-	memset(_rank, 0, sizeof(_rank));
-	for(int i=0;i<setSize;i++) {
-		_parId[i] = i;
+DSU::DSU(Tsz unionSize): 
+_unionSize((unionSize > max_size) ? 0 : unionSize), _nodes(new Union[_unionSize]) {
+	if(unionSize > max_size) error.print("Error: \"dataSize\"参数设置过大，不能超过\"max_size\"！");
+	for(int i=0;i<_unionSize;i++) {
+		_nodes[i].parent = _nodes + i;
+		_nodes[i].rank = 1;
+		_nodes[i].nodeList.push_back(i);
+		_set.insert(_nodes + i);
 	}
 }
 
@@ -29,52 +31,146 @@ _setSize((setSize > max_size) ? 0 : setSize), _parId(new Tsz[_setSize]), _rank(n
  *	@brief	并查集 DSU类 的析构函数
 **/
 DSU::~DSU() {
-	delete [] _parId;
-	delete [] _rank;
+	delete [] _nodes;
 }
 
 /**
- *	@brief	返回并查集的容量
- *	@return 当前并查集的容量
+ *	@brief	返回并查集当前的容量
+ *	@return 并查集的容量
 **/
 inline
 DSU::Tsz DSU::size() {
-    return _setSize;
+    return _unionSize;
+}
+
+/**
+ *	@brief	返回并查集当前的集合数目
+ *	@return 集合的个数
+**/
+inline
+DSU::Tsz DSU::num_union() {
+    return _set.size();
+}
+
+/**
+ *	@brief	返回当前节点所在集合的大小
+ *	@param	Tsz x	: 节点编号
+ *	@return 当前所在集合的大小
+**/
+inline
+DSU::Tsz DSU::size_union(Tsz x) {
+	return findRoot(_nodes[x].parent)->nodeList.size();
+}
+
+/**
+ *	@brief	返回同一集合中的所有节点
+ *	@param	Tsz x	: 节点编号
+ *	@return 当前所在集合内的所有节点
+**/
+inline
+std::list<DSU::Tsz> DSU::search_siblings(Tsz x) {
+	return findRoot(_nodes[x].parent)->nodeList;
+}
+
+/**
+ *	@brief	返回最小集合内的节点个数
+ *	@return 最小集合的大小
+**/
+inline
+DSU::Tsz DSU::size_minSet() {
+	return (*_set.begin())->nodeList.size();
+}
+
+/**
+ *	@brief	以列表为容器返回最小集合内的所有节点
+ *	@return 最小集合的所有节点
+**/
+inline
+std::list<DSU::Tsz> DSU::search_minSet() {
+	return (*_set.begin())->nodeList;
+}
+
+/**
+ *	@brief	返回最大集合内的节点个数
+ *	@return 最大集合的大小
+**/
+inline
+DSU::Tsz DSU::size_maxSet() {
+	return (*_set.rbegin())->nodeList.size();
+}
+
+/**
+ *	@brief	以列表为容器返回最大集合内的所有节点
+ *	@return 最大集合的所有节点
+**/
+inline
+std::list<DSU::Tsz> DSU::search_maxSet() {
+	return (*_set.rbegin())->nodeList;
 }
 
 /**
  *	@brief	判断两个节点是否在一个集合中
- *	@param	Tsz x		: 节点1
- *	@param	Tsz y		: 节点2
+ *	@param	Tsz x	: 节点编号1
+ *	@param	Tsz y	: 节点编号2
  *	@return	当前节点的根节点
 **/
 inline 
 bool DSU::inSameSet(Tsz x, Tsz y) {
-	return findRoot(x) == findRoot(y);
-}
-
-/**
- *	@brief	查找当前节点的根节点
- *	@param	c_Tsz x		: 当前节点
- *	@return	当前节点的根节点
-**/
-DSU::Tsz DSU::findRoot(c_Tsz x) {
-	return (_parId[x] == x) ? x : _parId[x] = findRoot(_parId[x]);
+	return findRoot(_nodes[x].parent) == findRoot(_nodes[y].parent);
 }
 
 /**
  *	@brief	合并两个节点x、y
- *	@param	Tsz x		: 节点1
- *	@param	Tsz y		: 节点2
+ *	@param	Tsz x	: 节点编号1
+ *	@param	Tsz y	: 节点编号2
  *	@return	null
 **/
 void DSU::unite(Tsz x, Tsz y) {
-	x = findRoot(x);
-	y = findRoot(y);
-	if(x == y) return;
-	if(_rank[x] > _rank[y]) _parId[y] = x;
-	else {
-		_parId[x] = y;
-		if(_rank[x] == _rank[y]) _rank[y]++;
+	Union* node_x = findRoot(_nodes[x].parent);
+	Union* node_y = findRoot(_nodes[y].parent);
+	if(node_x == node_y) return;
+	// y合并给x
+	if(node_x->rank > node_y->rank) {
+		uniteData(node_x, node_y);
 	}
+	// x合并给y
+	else {
+		uniteData(node_y, node_x);
+		if(node_x->rank == node_y->rank) node_y->rank++;
+	}
+}
+
+/**
+ *	@brief	_set的比较函数，使用于union排序，方便查看并查集的整体情况
+ *	@param	const Union*const& x	: 节点1
+ *	@param	const Union*const& y	: 节点2
+ *	@return	比较结果
+**/
+bool DSU::compare::operator () (const Union*const& x, const Union*const& y) const {
+	if(x->nodeList.size() != y->nodeList.size()) return x->nodeList.size() < y->nodeList.size();
+	else return x < y;
+}
+
+/**
+ *	@brief	数据合并 y合并给x
+ *	@param	Union* node_x	: 节点1
+ *	@param	Union* node_y	: 节点2
+ *	@return	null
+**/
+void DSU::uniteData(Union* node_x, Union* node_y) {
+	_set.erase(node_x);
+	_set.erase(node_y);
+	node_y->parent = node_x;
+	node_x->nodeList.merge(node_y->nodeList);
+	// _nodes[y].nodeList.clear();
+	_set.insert(node_x);
+}
+
+/**
+ *	@brief	查找当前节点的根节点
+ *	@param	Union* x	: 当前节点
+ *	@return	当前节点的根节点
+**/
+DSU::Union* DSU::findRoot(Union* node_x) {
+	return (node_x->parent == node_x) ? node_x : node_x->parent = findRoot(node_x->parent);
 }
--- DisjointSetUnion/DSU.h
+++ DisjointSetUnion/DSU.h
@@ -2,49 +2,64 @@
  * @Author: NZB 
  * @Date: 2022-04-29 22:38:25 
  * @Last Modified by: NZB
- * @Last Modified time: 2022-05-02 00:11:27
+ * @Last Modified time: 2022-05-03 00:28:11
  */
 
 #ifndef DSU_H
 #define DSU_H
 
 #include <list>
+#include <set>
 
 /* 并查集类 DSU */
 class DSU {
 
 protected:
-    typedef unsigned int Tsz;     	// 并查集的最大容量
-    typedef const Tsz c_Tsz;    	// 常量Tsz
+	struct Union;		// 并查集内部节点结构
+	struct compare {
+		bool operator () (const Union*const&, const Union*const&) const;
+	};
+	// bool compare(const Union*const&, const Union*const&);	// _set的比较函数，使用于union排序，方便查看并查集的整体情况
+
+    typedef unsigned int Tsz;     		// 并查集的最大容量
+    typedef const Tsz c_Tsz;    		// 常量Tsz
+	typedef std::list<Tsz> list;		// 链表
+	typedef std::set<Union*, compare> set;		// union容器
 
 public:
-	DSU(Tsz setSize);
+	DSU(Tsz unionSize);
 	~DSU();
 
-	inline Tsz size();						// 返回 并查集容量
+	inline Tsz size();						// 返回 并查集中节点个数
+	inline Tsz num_union();					// 返回 集合的个数
+
+	inline Tsz size_union(Tsz x);			// 返回 当前节点所在集合的大小
+	inline list search_siblings(Tsz x);		// 返回 同一集合中的所有节点
+
+	inline Tsz size_minSet();				// 返回 最小集合的大小
+	inline list search_minSet();			// 返回 最小集合的所有节点
+	inline Tsz size_maxSet();				// 返回 最大集合的大小
+	inline list search_maxSet();			// 返回 最大集合的所有节点
+	
 	inline bool inSameSet(Tsz x, Tsz y);	// 判断 两个节点是否在一个集合中
-	Tsz findRoot(Tsz x);					// 查找 根节点
-	virtual void unite(Tsz x, Tsz y);		// 合并 两个节点
-
-protected:
-	const static Tsz max_size = 1e7;	// 并查集允许的最大容量
-	Tsz _setSize;						// 并查集容量
-	Tsz* const _parId;					// 存储 父节点编号
-	Tsz* const _rank;					// 存储 以此为根节点的树深
-};
-
-/* 拓展并查集类 DSU_plus */
-class DSU_plus : public DSU {
+	void unite(Tsz x, Tsz y);				// 合并 两个节点所在的集合
 
 public:
-	DSU_plus(Tsz setSize);
-	~DSU_plus();
+	static c_Tsz max_size = 1 << 22;	// 并查集允许的最大容量
+	c_Tsz _unionSize;					// 并查集容量
+	Union* const _nodes;				// 存储 所有集合信息
+	set _set;							// 动态存储 所有集合整体信息
 
-	std::list<Tsz> search_Siblings(Tsz x);	// 查找 同一集合中的其他节点
-	std::list<Tsz> search_BySet(Tsz x);		// 查找 所有的节点，按集合分类
+	void uniteData(Union* node_x, Union* node_y);	// 数据合并 y合并给x
+	Union* findRoot(Union* x);						// 查找 所在集合的根节点
+};
 
-private:
-	std::list<Tsz> x;
+/* 并查集内部 集合类 DSU */
+struct DSU::Union {
+
+	Union* parent;		// 存储 父节点的地址
+	Tsz rank;			// 存储 集合构成树的树深
+	list nodeList;		// 存储 集合内的所有节点，非根节点均为空
 };
 
 #endif
